b30f8b011ead4bc373aef9f5559886a0ce750aaa
cmoerl <136233026+cmoerl@users.noreply.github.com>
2023-09-20 16:02:32 +0000

    message

--- Common_Core/Libft/ft_strchr.c
+++ Common_Core/Libft/ft_strchr.c
@@ -43,12 +43,12 @@ char	*ft_strchr(const char *s, int c)
 {
 	while (*s != '\0')
 	{
-		if ((char)*s == (char)c)
+		if (*s == (char)c)
 			return ((char *)s);
 		s++;
 	}
-	if (c == 0)
-		return (char *)s;
+	if ((char)c == '\0')
+		return ((char *)s);
 	return (NULL);
 }
 
