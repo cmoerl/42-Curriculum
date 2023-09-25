4729bcd827ae5523f6eceb9a635dbc23f2a73cf1
cmoerl <136233026+cmoerl@users.noreply.github.com>
2023-09-25 16:35:30 +0000

    printf

---
+++ Common_Core/Printf/ft_printf.c
@@ -0,0 +1,46 @@
+#include "printf.h"
+#include "libft.h"
+
+int ft_count_arg(const char *format)
+{
+    int i;
+    int count;
+
+    i = 0;
+    count = 0;
+    while (format[i])
+    {
+        if (format[i] == '%' && (format[i + 1] == 'c'
+        || format[i + 1] == 's' || format[i + 1] == 'p'
+        || format[i + 1] == 'd' || format[i + 1] == 'i'
+        || format[i + 1] == 'u' || format[i + 1] == 'x'
+        || format[i + 1] == 'X' || format[i + 1] == '%'))
+        {
+            count++;
+            i++;
+        }
+        if (format[i])
+            i++;
+    }
+    return (count);
+}
+
+int ft_printf(const char *format, ...)
+{
+    int num_args;
+    va_list args;
+    int i;
+
+    i = 0;
+    num_args = ft_count_arg(format);
+    while (i < num_args)
+    {
+        while (format[j])
+        {
+            
+        }
+        i++;
+    }
+    else
+        return (0);
+}
