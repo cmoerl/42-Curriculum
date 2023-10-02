946b5cda3f984e07cdd3ec140267ee2cb65308c6
Carla Juliana Sturm <csturm@c1r2p10.42vienna.com>
2023-10-02 16:16:39 +0200

    unsigned int

--- Common_Core/Printf/ft_printf.h
+++ Common_Core/Printf/ft_printf.h
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   printf.h                                           :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: csturm <csturm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 17:03:30 by csturm            #+#    #+#             */
-/*   Updated: 2023/09/29 10:57:42 by csturm           ###   ########.fr       */
+/*   Updated: 2023/10/02 16:13:34 by csturm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@
 
 int	ft_print_char(int c);
 int	ft_print_hex(unsigned int n, int up);
-int	ft_print_int(int n);
+int	ft_print_int(unsigned int n);
 int	ft_print_ptr(void *p);
 int	ft_print_str(char *str);
 int	ft_printf(const char *format, ...);
