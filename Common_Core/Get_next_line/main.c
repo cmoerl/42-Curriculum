b0b30910e0d52acf2b8b2f9d3a48eb2abfb026f9
Carla Juliana Sturm <csturm@c1r1p9.42vienna.com>
2023-11-14 17:24:27 +0100

    version with main

--- Common_Core/Get_next_line/main.c
+++ Common_Core/Get_next_line/main.c
@@ -1,4 +1,5 @@
-/*
+#include "get_next_line.h"
+
 #include <stdio.h>
 #include <fcntl.h>
 
@@ -9,7 +10,7 @@ int	main(void)
 	char	*line;
 
 	i = 0;
-	file_des = open ("testfile3.txt", O_RDONLY);
+	file_des = open ("testfile5.txt", O_RDONLY);
 	while (i < 100)
 	{
 		line = get_next_line(file_des);
@@ -24,44 +25,3 @@ int	main(void)
 	close(file_des);
 	return (0);
 }
-
-#include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include "get_next_line.h"
-
-int main(void)
-{
-    int file_des;
-//    int new_file_des;
-    char *line;
-
-	file_des = open ("testfile3.txt", O_RDONLY);
-    // Duplicate file descriptor 1000 to a valid file descriptor (e.g., 0).
-    if ((dup2(file_des, 1000)) < 0)
-    {
-        perror("Failed to duplicate file descriptor");
-        return 1;
-    }
-
-	close(file_des);
-
-    // Call get_next_line with the duplicated file descriptor.
-    line = get_next_line(-1);
-    if (line)
-    {
-        printf("Test Case 1 (file descriptor 1000): %s\n", line);
-        free(line);
-    }
-    else
-    {
-        printf("Test Case 1 (file descriptor 1000): Returned NULL, as expected.\n");
-    }
-
-    // Close the duplicated file descriptor.
-    close(1000);
-
-    return 0;
-}
-*/
