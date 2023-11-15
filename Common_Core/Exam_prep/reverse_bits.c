ec7d76366495c2e8112718b45263cd0f0ce4affc
Carla Sturm <carla.sturm@hotmail.de>
2023-11-15 21:57:25 +0100

    all bit exercises

---
+++ Common_Core/Exam_prep/reverse_bits.c
@@ -0,0 +1,15 @@
+unsigned char   reverse_bits(unsigned char octet)
+{
+    unsigned char   res;
+    int             i;
+
+    res = 0;
+    i = 8;
+    while (i > 0)
+    {
+        res = res * 2 + (octet % 2);
+        octet = octet / 2;
+        i--;
+    }
+    return (res);
+}
