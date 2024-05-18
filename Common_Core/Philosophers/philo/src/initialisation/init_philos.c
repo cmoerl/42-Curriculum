0ab53fb3ec3ad31c671758731a3d86bff73cec06
Carla Sturm <carla.sturm@hotmail.de>
2024-05-18 17:41:38 +0200

    only valgrind issue left

--- Common_Core/Philosophers/philo/src/initialisation/init_philos.c
+++ Common_Core/Philosophers/philo/src/initialisation/init_philos.c
@@ -6,7 +6,7 @@
 /*   By: csturm <csturm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/06 17:24:13 by csturm            #+#    #+#             */
-/*   Updated: 2024/05/18 13:29:02 by csturm           ###   ########.fr       */
+/*   Updated: 2024/05/18 17:03:02 by csturm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,6 @@ int	init_philos(t_data *data)
 	{
 		data->philos[i].philo_no = i + 1;
 		data->philos[i].prev_meal = 0;
-		if (data->philos[i].prev_meal == -1)
-			return (error(ERR_TIME, data), 1);
 		data->philos[i].meals = 0;
 		data->philos[i].data = data;
 		data->philos[i].mutex_init = 0;
