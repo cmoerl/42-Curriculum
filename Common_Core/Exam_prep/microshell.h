bfc8e3b99e8e4b2458b1a92eeeff56559a1c6715
Carla Sturm <carla.sturm@hotmail.de>
2024-05-25 18:36:17 +0200

    my try for exam rank 04

---
+++ Common_Core/Exam_prep/microshell.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aismaili <aismaili@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 20:01:14 by aismaili          #+#    #+#             */
+/*   Updated: 2024/04/10 20:02:23 by aismaili         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+# define MICROSHELL_H
+
+# include <unistd.h>
+# include <string.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+
+#endif
---
+++ Common_Core/Exam_prep/microshell2.h
@@ -0,0 +1,9 @@
+#ifndef MICROSHELL2_H
+#define MICROSHELL2_H
+
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif
