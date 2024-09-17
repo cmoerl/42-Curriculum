00ab9459dcf3592780c9a5ed8a714fcc790b5606
Carla Sturm <carla.sturm@hotmail.de>
2024-09-17 10:35:30 +0200

    changed using std::cout to std::cout

--- Common_Core/CPP/Module_00/ex00/megaphone.cpp
+++ Common_Core/CPP/Module_00/ex00/megaphone.cpp
@@ -6,29 +6,27 @@
 /*   By: csturm <csturm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 11:25:55 by csturm            #+#    #+#             */
-/*   Updated: 2024/09/16 11:44:45 by csturm           ###   ########.fr       */
+/*   Updated: 2024/09/17 10:32:25 by csturm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
-using std::cout;
-
 int main(int argc, char **argv)
 {
     if (argc == 1)
     {
-        cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
         return (0);
     }
     for (int i = 1; argv[i] != NULL; i++)
     {
         for (int j = 0; argv[i][j] != '\0'; j++)
-            cout << static_cast<char>(toupper(argv[i][j]));
+            std::cout << static_cast<char>(toupper(argv[i][j]));
         if (argv[i + 1] != NULL)
-            cout << " ";
+            std::cout << " ";
     }
-    cout << "\n";
+    std::cout << "\n";
     return (0);
 }
