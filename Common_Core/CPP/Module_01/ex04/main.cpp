931d4f06c96af789782a7c376f79c48b4c62d99d
cmoerl <carla.sturm@hotmail.de>
2024-10-08 11:27:36 +0200

    finished ex04

--- Common_Core/CPP/Module_01/ex04/main.cpp
+++ Common_Core/CPP/Module_01/ex04/main.cpp
@@ -6,13 +6,19 @@
 /*   By: csturm <csturm@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 14:06:40 by csturm            #+#    #+#             */
-/*   Updated: 2024/10/07 14:17:13 by csturm           ###   ########.fr       */
+/*   Updated: 2024/10/08 11:26:36 by csturm           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib>
+
+std::string    replaceLine(std::string line, size_t pos, size_t len, std::string s2)
+{
+    return (line.substr(0, pos) + s2 + line.substr(pos + len));
+}
 
 int main(int argc, char **argv)
 {
@@ -21,19 +27,16 @@ int main(int argc, char **argv)
         std::cout << "This program takes three parameters: a file name and two strings\n";
         return (1);
     }
-    // Open the file
     std::ifstream file(argv[1]);
     if (!file.is_open())
     {
         std::cout << "Error: could not open file\n";
         return (1);
     }
-    // Read the file
     std::string line;
     std::string filename = argv[1];
     filename += ".replace";
-    std::ofstream
-    output(filename);
+    std::ofstream output(filename.c_str());
     if (!output.is_open())
     {
         std::cout << "Error: could not open output file\n";
@@ -46,7 +49,7 @@ int main(int argc, char **argv)
         size_t pos = 0;
         while ((pos = line.find(s1, pos)) != std::string::npos)
         {
-            line.replace(pos, s1.length(), s2); // replace not allowed
+            line = replaceLine(line, pos, s1.length(), s2);
             pos += s2.length();
         }
         output << line << std::endl;
@@ -55,3 +58,9 @@ int main(int argc, char **argv)
     output.close();
     return (0);
 }
+
+/* 
+    - std::ifstream --> used to read from files
+    - std::ofstream --> used to write to files
+    - std::fstream --> used to read and write to files
+*/
