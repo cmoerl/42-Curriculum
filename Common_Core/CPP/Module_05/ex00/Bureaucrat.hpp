e5dddad676eed78f887c9f9bddb445ae987564f9
cmoerl <carla.sturm@hotmail.de>
2024-11-07 13:00:55 +0100

    ex00 finished

--- Common_Core/CPP/Module_05/ex00/Bureaucrat.hpp
+++ Common_Core/CPP/Module_05/ex00/Bureaucrat.hpp
@@ -2,17 +2,20 @@
 #define BUREAUCRAT_HPP
 
 #include <string>
+#include <exception>
+#include <iostream>
 
 class Bureaucrat
 {
     private:
-        std::string const   name;
-        int                 grade;
+        std::string const   _name;
+        int                 _grade;
 
     public:
         Bureaucrat();
         Bureaucrat(const Bureaucrat &copy);
         Bureaucrat &operator = (const Bureaucrat &copy);
+        Bureaucrat(std::string name, int grade);
         ~Bureaucrat();
 
         std::string getName(void) const;
@@ -20,6 +23,19 @@ class Bureaucrat
         void        incrementGrade(void);
         void        decrementGrade(void);
 
+        class   GradeTooHighException: public std::exception {
+            public:
+                const char* what() const throw();
+
+        };
+        class   GradeTooLowException: public std::exception {
+            public:
+                const char* what() const throw();
+
+        };
+
 };
+
+std::ostream &operator<<(std::ostream &out, const Bureaucrat &b);
 
 #endif
---
+++ Common_Core/CPP/Module_05/ex00/main.cpp
@@ -0,0 +1,56 @@
+#include "Bureaucrat.hpp"
+
+int main()
+{
+    try
+    {
+        Bureaucrat employee("Hans", 150);
+
+        employee.incrementGrade();
+        std::cout << employee << std::endl;
+        employee.decrementGrade();
+        std::cout << employee << std::endl;
+        employee.decrementGrade();
+        std::cout << employee << std::endl;
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    try
+    {
+        Bureaucrat boss("Franz", 1);
+
+        boss.decrementGrade();
+        std::cout << boss << std::endl;
+        boss.incrementGrade();
+        std::cout << boss << std::endl;
+        boss.incrementGrade();
+        std::cout << boss << std::endl;
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    try
+    {
+        Bureaucrat ceo("Gans", 0);
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    try 
+    {
+        Bureaucrat intern("Grant", 151);
+    }
+    catch (std::exception & e)
+    {
+        std::cerr << "Exception: " << e.what() << std::endl;
+    }
+
+    return (0);
+}
