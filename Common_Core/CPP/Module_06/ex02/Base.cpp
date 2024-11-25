1c2765438d67ed83a77a2e992552776f9bc70624
cmoerl <carla.sturm@hotmail.de>
2024-11-25 13:25:33 +0100

    added a check for NULL Pointer and a try and catch block

--- Common_Core/CPP/Module_06/ex02/Base.cpp
+++ Common_Core/CPP/Module_06/ex02/Base.cpp
@@ -20,15 +20,33 @@ Base*   generate(void) {
 }
 
 void    identify(Base* p) {
-    if (dynamic_cast<A*>(p)) {
+    if (dynamic_cast<A*>(p) != NULL) {
         std::cout << "A" << std::endl;
-    } else if (dynamic_cast<B*>(p)) {
+    } else if (dynamic_cast<B*>(p) != NULL) {
         std::cout << "B" << std::endl;
-    } else if (dynamic_cast<C*>(p)) {
+    } else if (dynamic_cast<C*>(p) != NULL) {
         std::cout << "C" << std::endl;
+    }
+    else {
+        std::cout << "Unknown" << std::endl;
     }
 }
 
 void    identify(Base& p) {
-    identify (&p);
+    try {
+        (void)dynamic_cast <A&>(p);
+        std::cout << "A" << std::endl;
+    } catch (std::exception &e) {
+        try {
+            (void)dynamic_cast <B&>(p);
+            std::cout << "B" << std::endl;
+        } catch (std::exception &e) {
+            try {
+                (void)dynamic_cast <C&>(p);
+                std::cout << "C" << std::endl;
+            } catch (std::exception &e) {
+                std::cout << "Unknown" << std::endl;
+            }
+        }
+    }
 }
