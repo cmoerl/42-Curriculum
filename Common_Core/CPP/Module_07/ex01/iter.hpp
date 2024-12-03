297c9499cb56744d2492fa8bcd15c26d019b605d
cmoerl <carla.sturm@hotmail.de>
2024-12-03 11:23:02 +0100

    improved ex01

--- Common_Core/CPP/Module_07/ex01/iter.hpp
+++ Common_Core/CPP/Module_07/ex01/iter.hpp
@@ -2,10 +2,11 @@
 #define ITER_HPP
 
 #include <string>
+#include <cstddef>
 
 template <typename T>
-void    iter(T* address, int len, void (*fun)(T const &)) {
-    for (int i = 0; i < len; i++)
+void    iter(T* address, size_t len, void (*fun)(T const &)) {
+    for (size_t i = 0; i < len; i++)
         fun(address[i]);
 }
 
--- Common_Core/CPP/Module_07/ex01/main.cpp
+++ Common_Core/CPP/Module_07/ex01/main.cpp
@@ -10,9 +10,11 @@ int main()
 {
     char   arr1[] = {'a', 'b', 'c', 'd', 'e'};
     int    arr2[] = {1, 2, 3, 4, 5};
+    double arr3[] = {1.1, 2.2, 3.3, 4.4, 5.5};
 
     iter(arr1, 5, print);
     iter(arr2, 5, print);
+    iter(arr3, 5, print);
 
     return (0);
 }
