94a165c0d93dbdb18530a8b64e3067252e7529da
cmoerl <carla.sturm@hotmail.de>
2025-01-15 14:28:56 +0100

    changed error message

--- Common_Core/CPP/Module_09/ex01/main.cpp
+++ Common_Core/CPP/Module_09/ex01/main.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv)
 {
     if (argc != 2) {
-        std::cerr << "Usage: ./rpn <expression>" << std::endl;
+        std::cerr << "Usage: ./RPN <expression>" << std::endl;
         return (1);
     }
 
