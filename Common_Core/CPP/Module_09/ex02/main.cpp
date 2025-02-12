46af509af646eca11f09cf1dd8cc280fbf1ac386
Carla Sturm <carla.sturm@hotmail.de>
2025-02-12 15:28:58 +0100

    added tests

--- Common_Core/CPP/Module_09/ex02/main.cpp
+++ Common_Core/CPP/Module_09/ex02/main.cpp
@@ -3,10 +3,68 @@
 int main() {
     PmergeMe    pmm;
 
-    pmm.initVec("8 9 10 11 12 13 14 15 1 2 3 4 5 6 7");
-    pmm.sortVec();
-    pmm.printVec();
-    // pmm.printTimeVec();
+    try {
+        pmm.initVec("8 9 10 11 12 13 14 15 1 2 3 4 5 6 7");
+        std::cout << "Before: ";
+        pmm.printVec();
+        pmm.sortVec();
+        std::cout << "After: ";
+        pmm.printVec();
+        pmm.printTimeVec();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+// add error handling for already sorted?
+    try {
+        pmm.initVec("1 2 3");
+        std::cout << "Before: ";
+        pmm.printVec();
+        pmm.sortVec();
+        std::cout << "After: ";
+        pmm.printVec();
+        pmm.printTimeVec();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+    try {
+        pmm.initVec("-1");
+        std::cout << "Before: ";
+        pmm.printVec();
+        pmm.sortVec();
+        std::cout << "After: ";
+        pmm.printVec();
+        pmm.printTimeVec();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+// add error handling for empty string
+    try {
+        pmm.initVec("");
+        std::cout << "Before: ";
+        pmm.printVec();
+        pmm.sortVec();
+        std::cout << "After: ";
+        pmm.printVec();
+        pmm.printTimeVec();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
+
+// added zeros!!
+    try {
+        pmm.initVec("100 7 42 45");
+        std::cout << "Before: ";
+        pmm.printVec();
+        pmm.sortVec();
+        std::cout << "After: ";
+        pmm.printVec();
+        pmm.printTimeVec();
+    } catch (std::exception &e) {
+        std::cout << e.what() << std::endl;
+    }
 
     return 0;
 }
